169268e9efbb3cb4da4bc82bfe0ccb4b683eb7f3
sguisse <sebguisse@gmail.com>
2018-11-09 08:38:58 +0100

    Sauvegarde initiale

--- src/main/java/com/sgu/formation/customers/biz/collaborateur/entity/Collaborateur.java
+++ src/main/java/com/sgu/formation/customers/biz/collaborateur/entity/Collaborateur.java
@@ -1,8 +1,14 @@
 package com.sgu.formation.customers.biz.collaborateur.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Getter
+@Setter
 public class Collaborateur {
+   
+   private Integer id;
 
 }
